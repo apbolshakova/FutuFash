9c912a56a77533175b2d68cd90626af174a6d2db
julody <julody@mail.ru>
2019-12-03 18:45:17 +0300

    Реализован класс Menu

--- FutuFash/Menu.cpp
+++ FutuFash/Menu.cpp
@@ -1,12 +1,34 @@
 #include "Menu.h"
 
 
-
-Menu::Menu()
+int Menu::getOperationCode(int min = 0, int max = 0)
 {
+	if (!min || !max) throw new std::exception("Invalid operations range.");
+	int opCode = 0;
+	do 
+	{
+		std::cout << "Enter number of required operation: ";
+		std::cin >> opCode;
+
+	} while (min > opCode || max < opCode);
+	return opCode;
 }
 
 
-Menu::~Menu()
+void Menu::handleWarning()
 {
+	const int confCode = 0x0D;
+	const int abortCode = 0x1B;
+	char ch = 0;
+	std::cout << "Are you sure? Press Enter to confirm or Esc to abort." << std::endl;
+	do {
+		std::cin >> ch;
+	} while (ch != confCode && ch != abortCode);
+	if (ch == abortCode) throw new std::exception("Operation was aborted.");
+}
+
+
+void Menu::printSuccessMessage()
+{
+	std::cout << "Success." << std::endl;
 }
--- FutuFash/Menu.h
+++ FutuFash/Menu.h
@@ -1,8 +1,13 @@
 #pragma once
+#include "Common.h"
+
+
 class Menu
 {
-public:
-	Menu();
-	~Menu();
+protected:
+	int getOperationCode(int min = 0, int max = 0);
+	virtual void printMenu() {};
+	void handleWarning();
+	void printSuccessMessage();
 };
 
