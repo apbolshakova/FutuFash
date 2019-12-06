af05708cdffbca36b1cc3abf042697da808e8edc
Елизавета Цыдыпова <tsydypova.eb@edu.spbstu.ru>
2019-12-06 19:52:42 +0300

    Реализован класс UserMenu

--- FutuFash/UserMenu.h
+++ FutuFash/UserMenu.h
@@ -1,10 +1,38 @@
 #pragma once
 #include "Common.h"
+#include "User.h"
+#include "Designer.h"
+#include "Model.h"
+#include "Menu.h"
+using namespace std;
 class UserMenu :
 	public Menu
 {
+private:
+	map<int, User*> users;
+	enum UserMenuMode {
+		GLOBAL_CHANGING,
+		PROJECT_CHANGING
+	}mode;
+	Project* projectToEdit;
+	void printMenu();
+	//switch
+	void handleAdding();
+	void printAllUsers();
+	void handleSearch();
+	//adding
+	void addNew(User* user);
+	//profile
+	int getNumToShow();
+	void printProfile(User* user);
+	void handleGlobalChanging(User* user);
+	void handleGlobalDeleting(User* user);
+	void handleProjectAdding(User* user);
+	void handleProjectDeleting(User* user);
 public:
-	UserMenu();
+	UserMenu(map<int, User*> *users, UserMenuMode mode, Project* projectToEdit);
 	~UserMenu();
 };
+
+
 
